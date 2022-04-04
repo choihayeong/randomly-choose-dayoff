836fe64054c4dff6f36ad1e5f701ec5a3d796c85
퍼블리싱 <chy7839@bdot.co.kr>
2022-04-04 17:20:25 +0900

    modify 마크업 수정, 지나간 달 선택시 랜덤 버튼 비활성화 등 수정

--- js/common.js
+++ js/common.js
@@ -1,54 +1,94 @@
-// common.js
-var today = new Date();     // 오늘 날짜
-var date = new Date();      // today의 Date를 세어주는 역할
+// import { LunarCalendar } from 'LunarCalendar.js';
 
-// 이전 달 이동
+let currentDate = new Date();
+const date = new Date();      // today의 Date를 세어주는 역할
+
+const calPrevBtn = document.querySelector('.btn-calprev');
+const calNextBtn = document.querySelector('.btn-calnext');
+const randomBtn = document.querySelector('#randomBtn');
+
+// 랜덤 선택 버튼 활성화/비활성화
+function disabledRndBtn(currentDate) {
+    if (currentDate < date) {
+        if (currentDate.getMonth() === date.getMonth() && currentDate.getFullYear() === date.getFullYear()) {
+            randomBtn.removeAttribute('disabled');
+        } else {
+            randomBtn.disabled = "true";
+        }
+    } else {
+        randomBtn.removeAttribute('disabled');
+    }
+}
+// move to prevMonth / nextMonth
 function prevCalendar() {   
-    today = new Date(today.getFullYear(), today.getMonth() - 1, today.getDate());
+    currentDate = new Date(currentDate.getFullYear(), currentDate.getMonth() - 1, currentDate.getDate());
     buildCalendar();
+    disabledRndBtn(currentDate);
 }
-// 다음 달 이동
 function nextCalendar() {   
-    today = new Date(today.getFullYear(), today.getMonth() + 1, today.getDate());
+    currentDate = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, currentDate.getDate());
     buildCalendar();
+    disabledRndBtn(currentDate);
 }
 
-// 달력 만들기(현재 달 기준)
-function buildCalendar(){
-    var firstDate = new Date(today.getFullYear(),today.getMonth(), 1);
-    var lastDate = new Date(today.getFullYear(),today.getMonth() + 1, 0);
+calPrevBtn.addEventListener('click', prevCalendar);
+calNextBtn.addEventListener('click', nextCalendar);
 
-    var tbCalendar = document.getElementById("calendar");
-    var tbCalendarYM = document.getElementById("tbCalendarYM");
-    tbCalendarYM.innerHTML = today.getFullYear() + "년 " + (today.getMonth() + 1) + "월"; 
+// randomBtn click event
+let datesArr;
+datesArr = buildCalendar();
+
+const modal = document.querySelector('#modal');
+const alertMsg = modal.querySelector('#alert');
+
+function randomBtnEvent() {
+    datesArr = buildCalendar();
+    const chosenDate = datesArr[Math.floor(Math.random() * datesArr.length)];
+
+    modal.classList.add('active');
+    alertMsg.innerHTML = `<span>${chosenDate}</span>일이 선택되었습니다!`;
+}
+function closeModalEvent() {
+    modal.classList.remove('active');
+}
+
+randomBtn.addEventListener('click', randomBtnEvent);
+modal.addEventListener('click', closeModalEvent);
+
+// generate calendar
+function buildCalendar(){
+    const firstDate = new Date(currentDate.getFullYear(), currentDate.getMonth(), 1);
+    const lastDate = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 0);
+
+    const calTable = document.querySelector("#calTable tbody");
+    const currentYM = document.querySelector("#currentYM");
+    currentYM.textContent = currentDate.getFullYear() + "년 " + (currentDate.getMonth() + 1) + "월"; 
 
     /* 이번달이 끝나면 다음달로 넘겨주는 역할 */
-    while (tbCalendar.rows.length > 2) {
-        tbCalendar.deleteRow(tbCalendar.rows.length - 1); //열을 지워줌. 기본 열 크기는 body 부분에서 2로 고정되어 있다.
-        //테이블의 tr 갯수 만큼의 열 묶음은 -1칸 해줘야지 30일 이후로 담을 달에 순서대로 열이 계속 이어진다.
+    while (calTable.rows.length > 0) {
+        calTable.deleteRow(calTable.rows.length - 1);
     }
 
-    var cnt = 0;    // 셀의 갯수
-    var row = null;
-    row = tbCalendar.insertRow();   // 테이블에 새로운 열 삽입 즉, 초기화
+    let cnt = 0;
+    let row = null;
+    let cell = null;
+    row = calTable.insertRow();
 
-    // 1일이 시작되는 칸을 맞추어 줌
     for (i = 0; i < firstDate.getDay(); i++) {
-        cell = row.insertCell();    // 열 한칸한칸 계속 만들어주는 역할
-        cnt = cnt + 1;              // 열의 갯수를 계속 다음으로 위치하게 해주는 역할
+        cell = row.insertCell();
+        cnt = cnt + 1;
     }
 
-    // thisMonthDates = thisMonthDates - disableDates
     let todayDate;
-    let thisMonthDates = []; // 해당 달의 전체 일을 넣고 공휴일을 빼는 배열
-    let disableDates = []; // 1일부터 오늘 날짜까지 포함, 토/일요일, 법정공휴일 데이터가 들어감 (Array)
+    let thisMonthDates = [];
+    let disableDates = [];
 
     // 법정 공휴일 구하기
-    var year = today.getFullYear();
-    var lastYear = 0;
+    const year = currentDate.getFullYear();
+    let lastYear = 0;
 
     // year와 lastyear가 동일할경우 setLunarToSolar를 계산하지 않음
-    var isSame = false;
+    let isSame = false;
     if (year == lastYear) {
         isSame = true;
     }
@@ -56,7 +96,7 @@ function buildCalendar(){
     const solarHolidays = ['0101','0301','0505','0606','0815','1003','1009','1225'];
     const lunarHolidays = ['0101','0102','0408','0814','0815','0816'];
     //대체공휴일 입력구간. 임시휴일이나 대체공휴일이 있을 경우 배열에 넣으면됨. yyyymmdd 입력
-    var alternativeHolidays = [ "20150929", "20160210", "20170130", "20180926", "20180507", "20190506", "20200127", "20220309", "20220601", "20220912", "20221010", "20230124", "20240212", "20240506", "20251008", "20270209", "20290924", "20290507", "20300205", "20300506", "20320921", "20330202", "20340221", "20350918", "20360130" ];
+    let alternativeHolidays = [ "20150929", "20160210", "20170130", "20180926", "20180507", "20190506", "20200127", "20220309", "20220601", "20220912", "20221010", "20230124", "20240212", "20240506", "20251008", "20270209", "20290924", "20290507", "20300205", "20300506", "20320921", "20330202", "20340221", "20350918", "20360130" ];
 
     let setLunarToSolar = [];
     let holidays = [];
@@ -67,97 +107,106 @@ function buildCalendar(){
         for (i = 0; i < lunarHolidays.length; i++) {
             var solar = Resut(year + "" + lunarHolidays[i]);
             if (i == 0) {
-                //var cDate = new Date();
-                var cMonth = solar.substring(0, 2);
-                var cDay = solar.substring(2, 4);
-                var cDate = new Date(parseInt(cMonth) + "/" + parseInt(cDay) + "/" + year);
+                let cMonth = solar.substring(0, 2);
+                let cDay = solar.substring(2, 4);
+                let cDate = new Date(parseInt(cMonth) + "/" + parseInt(cDay) + "/" + year);
 
                 cDate.setDate(cDate.getDate() - 1); // 하루전
 
-                /* cDate.setMonth(parseInt(cMonth-1)); // 월 설정
-                cDate.setDate(parseInt(cDay)); //일 설정
-                //cDate.setDate(-1); //하루전날 
-                //var sdate = cDate.setDate(cDate.getDate()-1); */
+                // cDate.setMonth(parseInt(cMonth-1)); // 월 설정
+                // cDate.setDate(parseInt(cDay)); //일 설정
+                // //cDate.setDate(-1); //하루전날 
+                // //var sdate = cDate.setDate(cDate.getDate()-1);
 
-                var sm = (cDate.getMonth() + 1);
+                let sm = (cDate.getMonth() + 1);
                 if (sm < 10) {
                     sm = "0" + sm;
                 }
 
-                var sd = (cDate.getDate());
+                let sd = (cDate.getDate());
                 if (sd < 10) {
                     sd = "0" + sd;
                 }
-                sDate = sm + "" + sd;
+                let sDate = sm + "" + sd;
                 setLunarToSolar.push(sDate);
             }
             setLunarToSolar.push(solar);
         }
 
-        lastYear = today.getFullYear();
+        lastYear = currentDate.getFullYear();
     }
 
-    if ((today.getMonth() + 1) < 10) {
-        month1 = "0" + (today.getMonth() + 1);
+    let monthStr;
+
+    if ((currentDate.getMonth() + 1) < 10) {
+        monthStr = "0" + (currentDate.getMonth() + 1);
     } else {
-        month1 = today.getMonth() + 1;
+        monthStr = currentDate.getMonth() + 1;
     }
 
-    for (i = 0; i < solarHolidays.length; i++) { // 양력휴일 넣음
-        if (solarHolidays[i].substring(0, 2) == month1) {
-            holidays.push(parseInt(solarHolidays[i].substring(2, 4))); //휴일이 있을경우 holidays에 넣는다.
+    for (i = 0; i < solarHolidays.length; i++) {
+        if (solarHolidays[i].substring(0, 2) == monthStr) {
+            holidays.push(Number(solarHolidays[i].substring(2, 4)));
         }
     }
-
-    for (i = 0; i < setLunarToSolar.length; i++) { // 음력휴일 넣음
-        if (setLunarToSolar[i].substring(0, 2) == month1) {
-            holidays.push(parseInt(setLunarToSolar[i].substring(2, 4))); //휴일이 있을경우 holidays에 넣는다.
+    for (i = 0; i < setLunarToSolar.length; i++) {
+        if (setLunarToSolar[i].substring(0, 2) == monthStr) {
+            holidays.push(Number(setLunarToSolar[i].substring(2, 4)));
         }
     }
-
-    for (i = 0; i < alternativeHolidays.length; i++) { //해당년도의 임시, 대체공휴일 추가
-        if (alternativeHolidays[i].substring(0, 4) == year) {//공휴년도가 해당년도와 일치할경우
-            if (alternativeHolidays[i].substring(4, 6) == month1) { //공휴년도가 해당월과 일치할경우
-                holidays.push(alternativeHolidays[i].substring(6, 8)); //일치하는 휴일이 있을경우 list에 넣는다.
+    for (i = 0; i < alternativeHolidays.length; i++) {
+        if (alternativeHolidays[i].substring(0, 4) == year) {
+            if (alternativeHolidays[i].substring(4, 6) == monthStr) {
+                holidays.push(Number(alternativeHolidays[i].substring(6, 8)));
             }
         }
     }
 
+    // asc-sorting for arr(holidays)
+    holidays.sort(function(a,b) {
+        return a-b;
+    });
+
     /* 달력 출력 */
     for (i = 1; i <= lastDate.getDate(); i++) { 
         thisMonthDates.push(i);
-        cell = row.insertCell();    // tr 추가
-        cell.innerHTML = i;
+        cell = row.insertCell();
+        // cell.textContent = i;
+        cell.innerHTML = `<button type="button">${i}</button>`;
         cnt = cnt + 1;
 
         /* 일요일 계산 */
-        if (cnt % 7 == 1) {     
-            cell.innerHTML = "<font color=#F79DC2>" + i
+        if (cnt % 7 == 1) {
+            cell.classList.add('sun');
+            cell.textContent = i;
             disableDates.push(i);
         }
         /* 토요일 구하기 */
-        if (cnt % 7 == 0){    
-            cell.innerHTML = "<font color=skyblue>" + i
-            row = calendar.insertRow();
+        if (cnt % 7 == 0){
+            cell.classList.add('sat');
+            // cell.textContent = i;
+            cell.innerHTML = `<button type="button">${i}</button>`;
+            row = calTable.insertRow();
             disableDates.push(i);
         }
         /* 법정 공휴일에 빨간색 */
         for (let j = 0; j < holidays.length; j++) {
             if (i == holidays[j]) {
-                cell.innerHTML = "<font color=#F79DC2>" + i;
+                cell.classList.add('holiday');
+                // cell.textContent = i;
+                cell.innerHTML = `<button type="button">${i}</button>`;
             }
         }
-
         /* 오늘의 날짜에 노란색 칠하기 */
-        if (today.getFullYear() == date.getFullYear() && today.getMonth() == date.getMonth() && i == date.getDate()) {
-            cell.bgColor = "#FAF58C";
-            cell.innerHTML = "<font color=#2b4450>" + i;
-            cell.style.fontWeight = "bold";
+        if (currentDate.getFullYear() == date.getFullYear() && currentDate.getMonth() == date.getMonth() && i == date.getDate()) {
+            cell.classList.add("current-date");
+            // cell.textContent = i;
+            cell.innerHTML = `<button type="button">${i}</button>`;
             todayDate = i;
         }
     }
 
-    // 해당 월의 연차 쓸 수 없는 데이터 disableDates에 저장
+    // 해당 월의 선택할 수 없는 데이터 disableDates에 저장
     for (let i = firstDate.getDate(); i < todayDate + 1; i++) {
         disableDates.push(i);
     }
@@ -167,6 +216,11 @@ function buildCalendar(){
 
     disableDates = new Set(disableDates);
     disableDates = [...disableDates];
+
+    // asc-sorting for arr(disableDates)
+    disableDates.sort(function(a,b) {
+        return a-b;
+    });
     
     // 이번 달의 전체 일 중 disabledDate를 뺌
     for (let i = 0; i < disableDates.length; i++) {
@@ -175,31 +229,23 @@ function buildCalendar(){
                 thisMonthDates.splice(j,1);
                 j--;
             }
-        }   
+        }
+    }
+    
+    // show current Holidays
+    const holidayInfo = document.querySelector('#holidayInfo span');
+    
+    if (holidays.length !== 0) {
+        holidayInfo.textContent = `${firstDate.getMonth() + 1}월의 법정 공휴일 ${holidays}일`;
+    } else {
+        holidayInfo.textContent = `${firstDate.getMonth() + 1}월의 법정 공휴일은 없습니다.`;
     }
 
     // console 출력
-    // for (let i = 0; i < holidays.length; i++) {
-    //     console.log(`${firstDate.getMonth()+1}월의 법정 공휴일 ${holidays[i]}일`);
-    // }
-    // console.log(`${firstDate.getMonth()+1}월의 연차 쓸 수 있는 날: ${thisMonthDates}`);
+    /* for (let i = 0; i < holidays.length; i++) {
+        console.log(`${firstDate.getMonth()+1}월의 법정 공휴일 ${holidays[i]}일`);
+    }
+    console.log(`${firstDate.getMonth()+1}월의 연차 쓸 수 있는 날: ${thisMonthDates}`); */
 
-    // 화면 출력
-    const messageInfo = document.querySelector('#message02 span');
-    messageInfo.textContent = `${firstDate.getMonth()+1}월의 법정 공휴일 ${holidays}일`;
-
-    // 랜덤 날짜 선택함수
-    const randomBtn = document.querySelector('#randomBtn');
-    const modal = document.querySelector('#modal');
-
-    randomBtn.addEventListener('click', function() {
-        const chosenDate = thisMonthDates[Math.floor(Math.random() * thisMonthDates.length)];
-        const messageDate = document.querySelector('#message span');
-        modal.classList.add('active');
-        messageDate.textContent = `${chosenDate}`;
-    });
-
-    modal.addEventListener('click', function() {
-        modal.classList.remove('active');
-    });
+    return thisMonthDates;
 }
